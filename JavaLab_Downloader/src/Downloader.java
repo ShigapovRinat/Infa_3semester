6c785082594ee6df05d74874ca87f658dc244598
Rinat Shigapov <r89179060010@gmail.com>
2019-09-30 15:54:38 +0300

    JavaLab Downloader fixed

--- JavaLab_Downloader/src/Downloader.java
+++ JavaLab_Downloader/src/Downloader.java
@@ -5,18 +5,20 @@ import java.net.URL;
 
 public class Downloader extends Thread {
     private String urlStr;
+    private String fileName;
 //    private String fileDirectory;
 
-    public Downloader(String urlStr) {
+    public Downloader(String urlStr, String fileName) {
         this.urlStr = urlStr;
+        this.fileName = fileName;
 //        this.fileDirectory = fileDirectory;
     }
 
     public void download() throws IOException {
         URL url = new URL(urlStr);
         BufferedInputStream bis = new BufferedInputStream(url.openStream());
-        String fileName = urlStr.substring(urlStr.lastIndexOf('/') + 1);
-        FileOutputStream fis = new FileOutputStream(fileName);
+//        String fileName = urlStr.substring(urlStr.lastIndexOf('/') + 1);
+        FileOutputStream fis = new FileOutputStream(fileName + urlStr.substring(urlStr.lastIndexOf('.')));
         byte[] buffer = new byte[1024];
         int count = 0;
         while ((count = bis.read(buffer, 0, 1024)) != -1) {
--- JavaLab_Downloader/src/Main.java
+++ JavaLab_Downloader/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Downloader[] downloaders = new Downloader[args.length];
         for (int i = 0; i < args.length; i++) {
-            downloaders[i] = new Downloader(args[i]);
+            downloaders[i] = new Downloader(args[i], i + 1 + "");
             downloaders[i].start();
         }
         for (int i = 0; i < downloaders.length; i++) {
